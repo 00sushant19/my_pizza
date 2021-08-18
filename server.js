aa468f64b1be51db668b9d8106b361a127c802ec
sushant <00sushant19@gmail.com>
2021-08-19 05:12:16 +0530

    initial

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const app = express();
+const ejs = require('ejs')
+const expressLayout = require('express-ejs-layouts')
+const path = require('path')
+const PORT = process.env.PORT || 3000
+// above line is telling that if "process.env.PORT" is present then use it else tkae 3000 
+
+app.get('/', (req,res)=>{
+    res.render('home');
+})
+
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+// setting the path 
+app.set('view engine', 'ejs')
+// setting the template ejs
+app.listen(3000, () => {
+    console.log(`listening on port ${PORT}`)
+})
