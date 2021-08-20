ac516f33d37c05e0e20fb972437e246a628d5892
sushant <00sushant19@gmail.com>
2021-08-20 15:20:04 +0530

    add cart page and auth

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ const path = require('path')
 const PORT = process.env.PORT || 3000
 // above line is telling that if "process.env.PORT" is present then use it else tkae 3000 
 
-app.get('/', (req,res)=>{
-    res.render('home');
-})
-
 // assets
 app.use(express.static('public'))
 
@@ -19,6 +15,24 @@ app.set('views', path.join(__dirname, '/resources/views'))
 // setting the path 
 app.set('view engine', 'ejs')
 // setting the template ejs
+
+
+app.get('/', (req,res)=>{
+    res.render('home');
+})
+
+app.get('/cart', (req,res) => {
+    res.render('customers/cart')
+})
+
+app.get('/login',(req,res) => {
+    res.render('auth/login');
+})
+
+
+app.get('/register',(req,res) => {
+    res.render('auth/register');
+})
 app.listen(3000, () => {
     console.log(`listening on port ${PORT}`)
 })
